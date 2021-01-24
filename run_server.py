fee3df01c54a8dcb137584c75a3009e5a391117d
N1ls0x <nils@wrenger.net>
2021-01-24 19:09:24 +0000

    Fix dependencies

--- run_server.py
+++ run_server.py
@@ -1,11 +1,11 @@
 import cherrypy
 
-from agents.RandomAgent import RandomAgent
+from agents.FooooodAgent import FooooodAgent
 from environment.Battlesnake.server import BattlesnakeServer
 
 
-agent = RandomAgent()
-port = 8004
+agent = FooooodAgent()
+port = 80
 
 server = BattlesnakeServer(agent)
 cherrypy.config.update({"server.socket_host": "0.0.0.0"})
