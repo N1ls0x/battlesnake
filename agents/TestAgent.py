9f0532965bf07636ffa34160901ca343f7cefdbd
N1ls0x <nils@wrenger.com>
2021-02-19 16:33:36 +0100

    test 2

--- agents/TestAgent.py
+++ agents/TestAgent.py
@@ -16,7 +16,7 @@ class TestAgent(BaseAgent):
         return 'Test'
     
     def get_color(self) -> Optional[Tuple]:
-        return (0, 127, 12)
+        return (0, 127, 2)
 
     def start(self, game_info: GameInfo, turn: int, board: BoardState, you: Snake):
         pass
