e8b433a8c2fcb50061f49522874ab210b73590bb
N1ls0x <nils@wrenger.com>
2021-02-25 15:02:06 +0100

    modifiet PatrickAgent

--- agents/PatrickAgent.py
+++ agents/PatrickAgent.py
@@ -1,6 +1,7 @@
 from typing import Dict,Optional,Tuple
 from agents.BaseAgent import BaseAgent
 import numpy as np
+import random
 from environment.Battlesnake.model.GameInfo import GameInfo
 from environment.Battlesnake.model.MoveResult import MoveResult
 from environment.Battlesnake.model.Snake import Snake
@@ -10,10 +11,10 @@ from environment.Battlesnake.model.grid_map import GridMap
 from environment.Battlesnake.model.Occupant import Occupant
 
 
-class TestAgent(BaseAgent):
+class PatrickAgent(BaseAgent):
 
     def get_name(self):
-        return 'Test'
+        return 'Patrick'
     
     def get_color(self) -> Optional[Tuple]:
         return (0, 127, 20)
@@ -23,7 +24,7 @@ class TestAgent(BaseAgent):
 
     def move(self, game_info: GameInfo, turn: int, board: BoardState, you: Snake) -> MoveResult:
         possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+        random.choice(possible_moves)
         MoveResult(possible_moves)
 
     def end(self, game_info: GameInfo, turn: int, board: BoardState, you: Snake):
